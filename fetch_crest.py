c7331e4910a098636a5e162392890f0bccd394ca
John Purcell <locke.renard@gmail.com>
2014-09-27 11:48:51 -0600

    adding database globals

--- fetch_crest.py
+++ fetch_crest.py
@@ -10,8 +10,24 @@ from datetime import datetime
 conf = ConfigParser.ConfigParser()
 conf.read(['init.ini','init_local.ini'])
 
+####DB STUFF####
+db_host   = conf.get('GLOBALS','db_host')
+db_user   = conf.get('GLOBALS','db_user')
+db_pw     = conf.get('GLOBALS','db_pw')
+db_port   = int(conf.get('GLOBALS','db_port'))
+db_schema = conf.get('GLOBALS','db_schema')
+db_driver = conf.get('GLOBALS','db_driver')
+sde_schema  = conf.get('GLOBALS','sde_schema')
+
+data_conn = pypyodbc.connect('DRIVER={%s};SERVER=%s;PORT=%s;UID=%s;PWD=%s;DATABASE=%s' \
+	% (db_driver,db_host,db_port,db_user,db_pw,db_schema))
+data_curr = data_conn.cursor()
+sde_conn  = pypyodbc.connect('DRIVER={%s};SERVER=%s;PORT=%s;UID=%s;PWD=%s;DATABASE=%s' \
+	% (db_driver,db_host,db_port,db_user,db_pw,sde_schema))
+sde_curr  = sde_conn.cursor()
+
 def _initSQL():
-	None
+	global data_conn, data_curr, sde_conn, sde_curr
 
 def main():
 	_initSQL()
