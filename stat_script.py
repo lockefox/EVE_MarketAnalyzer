6d734f2d5b569c2b90265499696f8f22f0a2a583
John Purcell <locke.renard@gmail.com>
2014-09-28 18:56:47 -0600

    adding stat_script to repo
    will do stat reports off scraped data

---
+++ stat_script.py
@@ -0,0 +1,27 @@
+#!/Python27/python.exe
+
+import sys, gzip, StringIO, sys, math, os, getopt, time, json, socket
+from os import path
+import urllib2
+import ConfigParser
+import pypyodbc
+from datetime import datetime
+import numpy
+import scipy
+
+#####
+#
+#	3sig	= 0.13% (769)
+#	2.5sig	= 0.62%	(161)
+#	2sig	= 2.28%	(44)
+#	1.5sig	= 6.68% (15)
+#	1sig	= 15.87%(6)
+#	0.5sig	= 30.85%(3)
+#
+#####
+
+def main():
+	None
+
+if __name__ == "__main__":
+	main()
