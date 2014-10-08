ce4e16764a13956e4f880d7187cf982c869afd79
John Purcell <jpurcell@micron.com>
2014-10-08 10:58:21 -0600

    added more dynamic header generator (to go with more dynamic sigma utilities)

--- stat_script.py
+++ stat_script.py
@@ -22,6 +22,59 @@ conf.read(['init.ini','init_local.ini'])
 #####
 
 ##TODO: dynamic limits
+sigma_to_percentile = {
+	-5  :2.86652e-7 ,
+	-4.9:4.79183e-7 ,
+	-4.8:7.93328e-7 ,
+	-4.7:1.30081e-6 ,
+	-4.6:2.11245e-6 ,
+	-4.5:3.39767e-6 ,
+	-4.4:5.41254e-6 ,
+	-4.3:8.53991e-6 ,
+	-4.2:1.33457e-5 ,
+	-4.1:2.06575e-5 ,
+	-4  :3.16712e-5 ,
+	-3.9:4.80963e-5 ,
+	-3.8:7.2348e-5,
+	-3.7:0.0001078,
+	-3.6:0.000159109,
+	-3.5:0.000232629,
+	-3.4:0.000336929,
+	-3.3:0.000483424,
+	-3.2:0.000687138,
+	-3.1:0.000967603,
+	-3  :0.001349898,
+	-2.9:0.001865813,
+	-2.8:0.00255513,
+	-2.7:0.003466974,
+	-2.6:0.004661188,
+	-2.5:0.006209665,
+	-2.4:0.008197536,
+	-2.3:0.01072411 ,
+	-2.2:0.013903448,
+	-2.1:0.017864421,
+	-2  :0.022750132,
+	-1.9:0.02871656 ,
+	-1.8:0.035930319,
+	-1.7:0.044565463,
+	-1.6:0.054799292,
+	-1.5:0.066807201,
+	-1.4:0.080756659,
+	-1.3:0.096800485,
+	-1.2:0.11506967 ,
+	-1.1:0.135666061,
+	-1  :0.158655254,
+	-0.9:0.184060125,
+	-0.8:0.211855399,
+	-0.7:0.241963652,
+	-0.6:0.274253118,
+	-0.5:0.308537539,
+	-0.4:0.344578258,
+	-0.3:0.382088578,
+	-0.2:0.420740291,
+	-0.1:0.460172163,
+	0   :0.5
+}
 SIG_3P0 = 769
 SIG_2P5 = 161
 SIG_2P0 = 44
@@ -46,7 +99,7 @@ sde_conn  = pypyodbc.connect('DRIVER={%s};SERVER=%s;PORT=%s;UID=%s;PWD=%s;DATABA
 	% (db_driver,db_host,db_port,db_user,db_pw,sde_schema))
 sde_cur = sde_conn.cursor()
 
-def market_volume_report(region=10000002,debug=False):
+def market_volume_report(report_sigmas, region=10000002,debug=False):
 	global convert
 	
 	sde_cur.execute('''SELECT typeid,typename
@@ -82,27 +135,8 @@ def market_volume_report(region=10000002,debug=False):
 	print 'Crunching Stats'
 	print_array = []
 	header = []
-	header.append('typeid')
-	header.append('typename')
-	header.append('N')
-	header.append('MIN')
-	header.append('P10')
-	header.append('MED')
-	header.append('AVG')
-	header.append('P90')
-	header.append('MAX')
-	header.append('STD')
-	header.append('SIG_N2P5')
-	header.append('SIG_N2P0')
-	header.append('SIG_N1P5')
-	header.append('SIG_N1P0')
-	header.append('SIG_N0P5')
-	header.append('SIG_0P0')
-	header.append('SIG_0P5')
-	header.append('SIG_1P0')
-	header.append('SIG_1P5')
-	header.append('SIG_2P0')
-	header.append('SIG_2P5')
+	header = build_header(report_sigmas)
+	
 	print_array.append(header)
 	
 	for itemID,vol_array in data_dict.iteritems():
@@ -196,6 +230,34 @@ def market_volume_report(region=10000002,debug=False):
 	
 	return_obj = dictify(print_array[0],print_array[1:])
 	return return_obj
+
+def build_header (report_sigmas,standard_stats = True)
+	header = []
+	header.append('typeid')
+	header.append('typename')
+	header.append('N')
+	if standard_stats:
+		header.append('MIN')
+		header.append('P10')
+		header.append('MED')
+		header.append('AVG')
+		header.append('P90')
+		header.append('MAX')
+		header.append('STD')
+	for sigma_num in report_sigmas:
+		sigma_str = 'SIG_'
+		try:
+			sigma_to_perfentile[sigma_num]
+		except KeyError as e:
+			print 'Sigma: %s not covered' % sigma_num
+			sys.exit(2)
+		
+		(decimal, integer) = math.modf(sigma_num)
+		if integer < 0:
+			sigma_str = '%sN' % sigma_str
+		
+		sigma_str = '%s%sP%s' % (sigma_str,int(abs(integer)),int(abs(decimal*10)))
+			#Like SIG_N2P5 or SIG_2P5
 	
 def dictify(header_list,data_list):
 	return_dict = {}
@@ -294,7 +356,28 @@ def sigma_report(market_sigmas, days, vol_floor = 100, region=10000002,debug=Fal
 	return result_dict			
 
 def main():
-	market_sigmas = market_volume_report()
+	report_sigmas = [
+		-2.5,
+		-2.0,
+		-1.5,
+		-1.0,
+		-0.5,
+		 0.0,
+		 0.5,
+		 1.0,
+		 1.5,
+		 2.0,
+		 2.5
+	]
+	
+	filter_sigmas = [
+		-2.5,
+		-2.0,
+		 2.0,
+		 2.5
+	]
+	
+	market_sigmas = market_volume_report(report_sigmas)
 	flaged_items = sigma_report(market_sigmas, 15)
 	
 	#print flaged_items
