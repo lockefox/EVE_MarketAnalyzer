a5818ef7c5a4efae32be7297b7deb27475a07458
John Purcell <locke.renard@gmail.com>
2014-10-11 14:44:32 -0600

    adding rpy_test to move R stuff into python

---
+++ rpy_test.py
@@ -0,0 +1,33 @@
+#!/Python27/python.exe
+
+import sys, gzip, StringIO, sys, math, os, getopt, time, json, socket
+from os import path
+import urllib2
+import ConfigParser
+import pypyodbc
+from datetime import datetime
+import numpy
+import rpy2.robjects as robjects
+
+conf = ConfigParser.ConfigParser()
+conf.read(['init.ini','init_local.ini'])
+
+db_host   = conf.get('GLOBALS','db_host')
+db_user   = conf.get('GLOBALS','db_user')
+db_pw     = conf.get('GLOBALS','db_pw')
+db_port   = int(conf.get('GLOBALS','db_port'))
+db_schema = conf.get('GLOBALS','db_schema')
+db_driver = conf.get('GLOBALS','db_driver')
+sde_schema  = conf.get('GLOBALS','sde_schema')
+
+sde_conn  = pypyodbc.connect('DRIVER={%s};SERVER=%s;PORT=%s;UID=%s;PWD=%s;DATABASE=%s' \
+	% (db_driver,db_host,db_port,db_user,db_pw,sde_schema))
+sde_cur = sde_conn.cursor()
+
+convert = {}
+
+def main():
+	None
+	
+if __name__ == "__main__":
+	main()
