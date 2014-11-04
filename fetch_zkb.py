68be61838c9dd55a3b21a10d921059f8176dc911
John Purcell <jpurcell@micron.com>
2014-11-04 16:25:52 -0700

    adding fetch_zkb script (like fetch_crest)
    also updated init.ini hooks for zkb locations

---
+++ fetch_zkb.py
@@ -0,0 +1,8 @@
+import sys, gzip, StringIO, sys, math, os, getopt, time, json, socket
+from os import path, environ
+import urllib2
+import ConfigParser
+import pypyodbc
+from datetime import datetime, timedelta
+import zkb
+
