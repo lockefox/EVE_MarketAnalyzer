32e301f7b5d5c7c32c19a0bed47848063ffaacc0
John Purcell <jpurcell@micron.com>
2014-11-11 17:10:09 -0700

    merged fetch_crest from etienne branch
    includes fix for 404 abort case

--- fetch_crest.py
+++ fetch_crest.py
@@ -3,53 +3,14 @@
 import sys, gzip, StringIO, sys, math, os, getopt, time, json, socket
 from os import path, environ
 import urllib2
-import ConfigParser
+import httplib
 import pypyodbc
 from datetime import datetime, timedelta
 import _strptime # because threading
 import threading
 
-import itertools
-flatten = itertools.chain.from_iterable
-
-conf = ConfigParser.ConfigParser()
-conf.read(['init.ini','init_local.ini'])
-####GLOBALS####
-crest_path = conf.get('CREST','default_path')
-crest_test_path = conf.get('CREST','test_path')
-user_agent = conf.get('GLOBALS','user_agent')
-retry_limit = int(conf.get('GLOBALS','default_retries'))
-sleep_timer = int(conf.get('GLOBALS','default_sleep'))
-crash_filename_base = conf.get('CREST','progress_file_base')
-tick_delay = timedelta(seconds=10)
-tick_delay_dbg = timedelta(seconds=5)
-
-####DB STUFF####
-db_host   = conf.get('GLOBALS','db_host')
-db_user   = conf.get('GLOBALS','db_user')
-db_pw     = environ.get('MYSQL_%s_PW' % db_user.upper(), '')
-db_pw     = db_pw if db_pw else conf.get('GLOBALS','db_pw')
-db_port   = int(conf.get('GLOBALS','db_port'))
-db_schema = conf.get('GLOBALS','db_schema')
-db_driver = conf.get('GLOBALS','db_driver')
-sde_schema  = conf.get('GLOBALS','sde_schema')
-
-def connect_local_databases(*args):
-	global db_driver, db_host, db_port, db_user, db_pw, db_schema, sde_schema
-	schemata = args if args else [db_schema, sde_schema]
-	connections = [
-		pypyodbc.connect(
-			'DRIVER={%s};SERVER=%s;PORT=%s;UID=%s;PWD=%s;DATABASE=%s' 
-			% (db_driver,db_host,db_port,db_user,db_pw,schema)
-			)
-		for schema in schemata
-		]
-	return flatten([conn, conn.cursor()] for conn in connections)
-
-####TABLES####
-crest_pricehistory  = conf.get('TABLES','crest_pricehistory')
-crest_industryindex = conf.get('TABLES','crest_industryindex')
-crest_serverprices  = conf.get('TABLES','crest_serverprices')
+from ema_config import *
+thread_exit_flag = False
 
 def thread_print(msg):
 	sys.stdout.write("%s\n" % msg)
@@ -76,84 +37,6 @@ def print_progress(timed_msg, last, start, count, total, debug=False):
 	else: 
 		return last 
 
-
-####CONST STUFF####
-region_list = {
-	'10000001':'Derelik',
-	'10000002':'The Forge',
-	'10000003':'Vale of the Silent',
-	'10000005':'Detorid',
-	'10000006':'Wicked Creek',
-	'10000007':'Cache',
-	'10000008':'Scalding Pass',
-	'10000009':'Insmother',
-	'10000010':'Tribute',
-	'10000011':'Great Wildlands',
-	'10000012':'Curse',
-	'10000013':'Malpais',
-	'10000014':'Catch',
-	'10000015':'Venal',
-	'10000016':'Lonetrek',
-	'10000018':'The Spire',
-	'10000020':'Tash-Murkon',
-	'10000021':'Outer Passage',
-	'10000022':'Stain',
-	'10000023':'Pure Blind',
-	'10000025':'Immensea',
-	'10000027':'Etherium Reach',
-	'10000028':'Molden Heath',
-	'10000029':'Geminate',
-	'10000030':'Heimatar',
-	'10000031':'Impass',
-	'10000032':'Sinq Laison',
-	'10000033':'The Citadel',
-	'10000034':'The Kalevala Expanse',
-	'10000035':'Deklein',
-	'10000036':'Devoid',
-	'10000037':'Everyshore',
-	'10000038':'The Bleak Lands',
-	'10000039':'Esoteria',
-	'10000040':'Oasa',
-	'10000041':'Syndicate',
-	'10000042':'Metropolis',
-	'10000043':'Domain',
-	'10000044':'Solitude',
-	'10000045':'Tenal',
-	'10000046':'Fade',
-	'10000047':'Providence',
-	'10000048':'Placid',
-	'10000049':'Khanid',
-	'10000050':'Querious',
-	'10000051':'Cloud Ring',
-	'10000052':'Kador',
-	'10000053':'Cobalt Edge',
-	'10000054':'Aridia',
-	'10000055':'Branch',
-	'10000056':'Feythabolis',
-	'10000057':'Outer Ring',
-	'10000058':'Fountain',
-	'10000059':'Paragon Soul',
-	'10000060':'Delve',
-	'10000061':'Tenerifis',
-	'10000062':'Omist',
-	'10000063':'Period Basis',
-	'10000064':'Essence',
-	'10000065':'Kor-Azor',
-	'10000066':'Perrigen Falls',
-	'10000067':'Genesis',
-	'10000068':'Verge Vendor',
-	'10000069':'Black Rise'
-	}
-
-trunc_region_list = {
-	'10000002':'The Forge',
-	'10000043':'Domain',
-	'10000032':'Sinq Laison',
-	'10000042':'Metropolis',
-	}
-
-region_name_maxlen = max( len( r ) for r in region_list.values() ) + 1
-region_name_format = "{:" + str(region_name_maxlen) + "s}"
 
 def _validate_connection(
 		tables=[crest_pricehistory, crest_industryindex, crest_serverprices],
@@ -224,16 +107,18 @@ def fetch_markethistory(regions={}, debug=False, testserver=False):
 			continue
 
 		for count,itemID in enumerate(item_list):
+			if thread_exit_flag: 
+				thread_print( fmt_name + "Received exit signal." )
+				return
 			last = print_progress_thread()
 			query = 'market/%s/types/%s/history/' % (regionID,itemID)
-			if itemID in crash_JSON['market_history'][regionID]:
-				if debug: thread_print( '%s:\tskip' % query )
+			if str(itemID) in crash_JSON['market_history'][regionID]:
+				thread_print( '%s:\tskip' % query )
 				continue #already processed data
 			
 			price_JSON = fetchURL_CREST(query, testserver, debug=False)
 			
 			if len(price_JSON['items']) == 0: 
-				write_progress('market_history',regionID,itemID,crash_JSON)
 				if debug: thread_print( '%s:\tEMPTY' % query )
 				continue
 			data_to_write = []
@@ -296,6 +181,7 @@ def fetchURL_CREST(query, testserver=False, debug=False):
 	request.add_header('User-Agent',user_agent)
 	
 	headers = {}
+	fatal_error = True
 	for tries in range (0,retry_limit):
 		time.sleep(sleep_timer*tries)
 		try:
@@ -305,12 +191,19 @@ def fetchURL_CREST(query, testserver=False, debug=False):
 			response = raw_response.read()
 		except urllib2.HTTPError as e:
 			thread_print( 'HTTPError:%s %s' % (e,real_query) )
+			fatal_error = False
 			continue
 		except urllib2.URLError as e:
 			thread_print( 'URLError:%s %s' % (e,real_query) )
+			fatal_error = True
+			continue
+		except httplib.HTTPException as e:
+			thread_print( 'HTTPException:%s %s' % (e, real_query) )
+			fatal_error = True
 			continue
 		except socket.error as e:
 			thread_print( 'Socket Error:%s %s' % (e,real_query) )
+			fatal_error = True
 			continue
 		
 		do_gzip = headers.get('Content-Encoding','') == 'gzip'
@@ -322,9 +215,11 @@ def fetchURL_CREST(query, testserver=False, debug=False):
 				return_result = json.load(zipper)
 			except ValueError as e:
 				thread_print( "Empty response: Retry %s" % real_query )
+				fatal_error = True
 				continue
 			except IOError as e:
 				thread_print( "gzip unreadable: Retry %s" % real_query )
+				fatal_error = True
 				continue
 			else:
 				break
@@ -333,7 +228,8 @@ def fetchURL_CREST(query, testserver=False, debug=False):
 			break
 	else:
 		thread_print( headers )
-		sys.exit(2)
+		if fatal_error: sys.exit(2)
+		return {'items':[]}
 	
 	return return_result
 	
@@ -348,17 +244,19 @@ def recover_on_restart(region_id):
 	try:
 		with open(crash_filename,'r') as f:
 			crash_JSON = json.load(f)
-		if ((not 'market_history' in crash_JSON) or 
-			(not region_id in crash_JSON['market_history'])
+		if (
+				(not 'market_history' in crash_JSON) or 
+				(not region_id in crash_JSON['market_history'])
 			):
 			raise Exception("Corrupted recovery file.")
-		msg = '%s loaded progress file %s' 
+		msg = '%s loaded progress file %s with %s items' 
 	except Exception as e:
-		msg = '%s found no progress file; starting fresh with %s'
+		msg = '%s found no progress file; starting fresh with %s and %s items'
 		crash_JSON['market_history'] = {}
 		crash_JSON['market_history'][region_id] = {}
 	crash_JSON['filename'] = crash_filename
-	thread_print( msg % (fmt_name, crash_filename) ) 
+	num_items = len(crash_JSON['market_history'][region_id])
+	thread_print( msg % (fmt_name, crash_filename, num_items) ) 
 	return crash_JSON
 
 def write_progress(subtable_name, key1, key2, crash_JSON):
@@ -384,7 +282,7 @@ def launch_region_threads(regions={}):
 			kwargs=kwargs, 
 			target=fetch_markethistory
 			)
-		new_thread.daemon = True
+		new_thread.daemon = False # So they can clean up properly
 		region_threads.append(new_thread)
 		new_thread.start()
 	return region_threads		
@@ -399,7 +297,7 @@ def wait_region_threads(threads=[]):
 			else: done.append(t.name)
 		if done:
 			last = print_progress(
-				",".join(done) + " finished. {elapsed:.2f} m total", 
+				", ".join(done) + " finished. {elapsed:.2f} m total", 
 				last, 
 				start, 
 				len(done), 
@@ -422,4 +320,8 @@ def main():
 	_optimize_database()
 
 if __name__ == "__main__":
-	main()
+	try:
+		main()
+	except KeyboardInterrupt:
+		thread_exit_flag = True
+		raise
