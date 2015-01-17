60623181922a3d31bcded872609c03ac3b831b72
John Purcell <jpurcell@micron.com>
2015-01-17 12:19:26 -0700

    updating framwork for cron_evecentral

--- cron_evecentral.py
+++ cron_evecentral.py
@@ -1,7 +1,43 @@
 import sys, gzip, StringIO, sys, math, os, getopt, time, json, socket
 from os import path, environ
 import urllib2
-import ema_config
 import pypyodbc
 from datetime import datetime, timedelta
+import threading
 
+from ema_config import *
+thread_exit_flag = False
+
+def thread_print(msg):
+	sys.stdout.write("%s\n" % msg)
+	sys.stdout.flush()
+	
+def query_locationType(locationID):
+	#Returns supported query modifier.  Else blank to avoid bad calls
+	digit = str(locationID)[-1]
+	int_digit = int(digit)
+	
+	if int_digit == 1:
+		return 'regionlimit=%s' % locationID
+	#elif  int_digit == 2:
+	#	None #constellation not supported
+	elif int_digit == 3:
+		return 'usesystem=%s' % locationID
+	elif int_digit == 6:
+		return 'usestation=%s' % locationID
+	else:
+		return ''
+def fetch_typeIDs():
+	None
+	
+def _initSQL(table_name):
+	None
+	
+def main:
+	None
+if __name__ == "__main__":
+	try:
+		main()
+	except KeyboardInterrupt:
+		thread_exit_flag = True
+		raise
