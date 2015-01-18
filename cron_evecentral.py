5d88af0c175ee6b9ab578d2e68f5c77ddcc6731a
Lockefox <locke.renard@gmail.com>
2015-01-17 22:40:16 -0700

    saving working state
    fetch URL routine works
    Working on writeSQL step.  Need to finalize commit statements

--- cron_evecentral.py
+++ cron_evecentral.py
@@ -18,6 +18,11 @@ itemlist = []
 snapshot_table = conf.get('TABLES','cron_evecentral')
 logfile_name = conf.get('CRON','evecentral_logfile') #add locationID to logfile name
 evecentral_url = conf.get('CRON','evecentral_baseURL')
+fetch_type = conf.get('CRON','evecentral_defaulttype')
+table_header = ''	#will be an issue if _initSQL is called multiple times
+start_datetime = datetime.utcnow()
+commit_date = start_datetime.strftime('%Y-%m-%d')
+commit_time = start_datetime.strftime('%H:%M:%S')
 
 def thread_print(msg):
 	sys.stdout.write("%s\n" % msg)
@@ -25,17 +30,16 @@ def thread_print(msg):
 	
 def query_locationType(locationID):
 	#Returns supported query modifier.  Else blank to avoid bad calls
-	digit = str(locationID)[-1]
+	digit = str(locationID)[:1]
 	int_digit = int(digit)
-	
 	if int_digit == 1:
-		return 'regionlimit=%s' % locationID
+		return 'regionlimit'
 	#elif  int_digit == 2:
 	#	None #constellation not supported
 	elif int_digit == 3:
-		return 'usesystem=%s' % locationID
+		return 'usesystem'
 	elif int_digit == 6:
-		return 'usestation=%s' % locationID
+		return 'usestation'
 	else:
 		return ''	#exception would be better
 
@@ -51,20 +55,19 @@ def fetch_typeIDs():
 
 	query_filename = conf.get('CRON','evecentral_query')
 	item_query = open(path.relpath('SQL/%s.mysql' % query_filename)).read()
-	print item_query
 	sde_cur.execute(item_query)
 	raw_values = sde_cur.fetchall()
 	
 	return_list = []
 	for row in raw_values:
-		return_list.append(row[0])
+		return_list.append(int(row[0]))
 	return return_list
 	
 def writelog(locationID, message):
 	None
 	
 def _initSQL(table_name):
-	global db_con, db_cur
+	global db_con, db_cur, table_header
 	
 	db_con = MySQLdb.connect(
 		host   = db_host,
@@ -89,27 +92,37 @@ def _initSQL(table_name):
 			print e[1]
 			sys.exit(2)
 		print '%s.%s:\tCREATED' % (db_schema,table_name)
-
+	db_cur.execute('''SHOW COLUMNS FROM `%s`''' % table_name)
+	raw_headers = db_cur.fetchall()
+	tmp_headers = []
+	for row in raw_headers:
+		tmp_headers.append(row[0])
+		
+	table_header = ','.join(tmp_headers)
+	
 def fetch_data(itemlist,locationID,debug=False):
 	fetch_scope = query_locationType(locationID)
+	itemid_str = ','.join(map(str,itemlist))
+	
+	fetch_url = "%s%s" % (evecentral_url,fetch_type) 
+	real_query = '%s?%s=%s&typeid=%s' % (fetch_url,fetch_scope,locationID,itemid_str)
+	#if debug: thread_print( real_query )
+
+	request = urllib2.Request(fetch_url)
+	request.add_header('Accept-Encoding','gzip')
+	request.add_header('User-Agent',user_agent)
+	request.add_header(fetch_scope,locationID)
+	request.add_header('typeid',itemid_str)
+	
 	#POST_values = {
 	#	'accept-encoding' : 'gzip',
 	#	fetch_scope       : locationID,
 	#	'user-agent'      : user_agent,
-	#	'typeid'          : ','.join(itemlist)
+	#	'typeid'          : itemid_str
 	#	}
 	#
 	#payload = urllib.urlencode(POST_values)
-	#request = urllib2.Request(evecentral_url,payload)
-	
-	real_query = '%s?%s=%s&typeid=%s' % (evecentral_url,fetch_scope,locationID,','.join(itemlist))
-	#if debug: thread_print( real_query )
-	
-	request = urllib2.Request(evecentral_url)
-	request.add_header('Accept-Encoding','gzip')
-	request.add_header('User-Agent',user_agent)
-	request.add_header(fetch_scope,locationID)
-	request.add_header('typeid',','.join(itemlist))
+	#request = urllib2.Request(fetch_url,payload)
 	
 	headers = {}
 	fatal_error = True
@@ -166,11 +179,55 @@ def fetch_data(itemlist,locationID,debug=False):
 		return {'items':[]}
 	
 	return return_result
+	
+def writeSQL(JSON_obj,locationID):
+	insert_statement = '''INSERT INTO %s (%s) VALUES''' % (snapshot_table, table_header)
+	
+	for item_info in JSON_obj:
+		for price_key,price_obj in item_info.iteritems():
+			buy_or_sell = 0
+			if price_key == "buy":
+				buy_or_sell = 0
+			elif price_key == "sell":
+				buy_or_sell = 1
+			else:
+				continue
+			best_price = None
+			if buy_or_sell:
+				best_price = price_obj['min']
+			else:
+				best_price = price_obj['max']
+			
+			insert_line = '''('%s','%s',%s,%s,'%s',%s,%s,%s,%s,%s)''' % (/
+				commit_date,\
+				commit_time,\
+				price_obj['forQuery']['types'][0],\
+				locationID,\
+				query_locationType(locationID),\
+				best_price,\
+				price_obj['avg'],\
+				price_obj['volume'])
+				
 def main():
 	_initSQL(snapshot_table)
 	
 	item_list = fetch_typeIDs()
-	print item_list
+	
+	locationID = 30000142
+	request_limit = int(conf.get('CRON','evecentral_typelimit'))
+	sub_list = []
+	for itemid in item_list:
+		sub_list.append(itemid)
+		if len(sub_list) >= request_limit:
+			return_JSON = fetch_data(sub_list,locationID)
+			print return_JSON
+			sys.exit()
+			writeSQL(return_JSON,locationID)
+			sub_list = []
+	if len(sub_list) > 0:
+		return_JSON = fetch_data(item_list,systemID)
+		writeSQL(return_JSON)
+			
 	
 if __name__ == "__main__":
 	try:
