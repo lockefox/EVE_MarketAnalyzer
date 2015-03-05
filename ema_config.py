f69a0966c6e83cb307d341dc638e160acd0a8560
Lockefox <locke.renard@gmail.com>
2015-03-04 21:35:09 -0700

    adding SDE lookup table for tracking EVE releases

--- ema_config.py
+++ ema_config.py
@@ -127,11 +127,11 @@ region_list = {
 
 trunc_region_list = {
 	'10000002':'The Forge',
-	'10000043':'Domain',
-	'10000030':'Heimatar',
-	'10000032':'Sinq Laison',
-	'10000042':'Metropolis',
-	'11000031':'Thera'
+	#'10000043':'Domain',
+	#'10000030':'Heimatar',
+	#'10000032':'Sinq Laison',
+	#'10000042':'Metropolis',
+	#'11000031':'Thera'
 	}
 
 region_name_maxlen = max( len( r ) for r in region_list.values() ) + 1
