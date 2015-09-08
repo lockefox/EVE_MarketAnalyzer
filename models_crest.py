a14ca74c6cfa5dc6f5e64cbc4802f1085e7871d4
Lockefox <locke.renard@gmail.com>
2015-09-07 19:21:25 -0600

    adding models_crest for framework
    TODO: add data pull/reshape to models_crest
    TODO: add framework to stat_script for crunching

---
+++ models_crest.py
@@ -0,0 +1,21 @@
+#!/Python27/python.exe
+
+import sys, gzip, StringIO, sys, math, os, getopt, time, json, socket
+from os import path
+import requests
+import ConfigParser
+import pypyodbc
+from datetime import datetime
+
+import pandas as pd
+from ema_config import *
+
+def main():
+	None
+
+if __name__ == "__main__":
+	try:
+		main()
+	except KeyboardInterrupt:
+		thread_exit_flag = True
+		raise
